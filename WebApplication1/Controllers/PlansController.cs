15e5729802f4bc0ed24ebcac62c8566f9a48f578
jasmine <jasminemohamed174005@gmail.com>
2026-05-13 19:41:46 +0300

    Add project files.

---
+++ WebApplication1/Controllers/PlansController.cs
@@ -0,0 +1,34 @@
+﻿using GYMProject.DBContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace GYMProject.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDBContext gymDBContext;
+        public PlansController()
+        {
+            gymDBContext = new GymDBContext();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var plans = await gymDBContext.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details (int id)
+        {
+            var plan = await gymDBContext.Plans.FindAsync( id);
+            if(plan is null)
+               return  RedirectToAction(nameof(Index));
+
+            else
+                return View(plan);
+            
+
+
+        }
+    }
+}
