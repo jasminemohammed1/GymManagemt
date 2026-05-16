e6f37121077297fdbe3af8a0060969643d99df9c
jasmine <jasminemohamed174005@gmail.com>
2026-05-16 18:39:49 +0300

    Add Member Repo

--- GymManagement.DAL/Repositories/Classes/MemberRepository.cs
+++ GymManagement.DAL/Repositories/Classes/MemberRepository.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using GymManagement.DAL.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using GYMProject.DBContexts;
+using GYMProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GymManagement.DAL.Repositories.Classes
 {
-    internal class MemberRepository
+    public class MemberRepository : IGymUserRepository<Member>
     {
+        private readonly GymDBContext _dbcontext;
+        public MemberRepository( GymDBContext db )
+        {
+            _dbcontext = db;
+        }
+
+        public async Task<int> AddAsync(Member user, CancellationToken ct)
+        {
+            _dbcontext.Members.Add(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+        }
+
+        public async Task<int> DeleteAsync(Member user, CancellationToken ct)
+        {
+            _dbcontext.Members.Remove(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+        }
+
+        public async Task<IEnumerable<Member>> GetAllAsync(bool tracking = false, CancellationToken ct = default)
+        {
+            IQueryable<Member> res = tracking ? _dbcontext.Members : _dbcontext.Members.AsNoTracking();
+            return await res.ToListAsync(ct);
+        }
+
+        public async Task<Member?> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            return await _dbcontext.Members.FindAsync(id, ct);
+
+        }
+
+        public  async Task<int> UpdateAsync(Member user, CancellationToken ct)
+        {
+            _dbcontext.Members.Update(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+        }
     }
 }
--- GymManagement.DAL/Repositories/Interfaces/IGymUserRepository.cs
+++ GymManagement.DAL/Repositories/Interfaces/IGymUserRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GymManagement.DAL.Models;
+using GYMProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,12 @@ using System.Threading.Tasks;
 
 namespace GymManagement.DAL.Repositories.Interfaces
 {
-    internal interface IGymUserRepository
+    internal interface IGymUserRepository<T> where T: GymUser
     {
-
+        public Task<IEnumerable<T>> GetAllAsync(bool tracking = false, CancellationToken ct = default);
+        public Task<T?> GetByIdAsync(int id, CancellationToken ct = default);
+        public Task<int> UpdateAsync(T user, CancellationToken ct);
+        public Task<int> AddAsync(T user, CancellationToken ct);
+        public Task<int> DeleteAsync(T user, CancellationToken ct);
     }
 }
