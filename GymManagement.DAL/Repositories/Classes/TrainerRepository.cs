33b91ccf180da748cfd0d74ba5bee8b06a0d0b8c
jasmine <jasminemohamed174005@gmail.com>
2026-05-16 18:46:40 +0300

    Add Trainer repo

--- GymManagement.DAL/Repositories/Classes/TrainerRepository.cs
+++ GymManagement.DAL/Repositories/Classes/TrainerRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GymManagement.DAL.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using GYMProject.DBContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,45 @@ using System.Threading.Tasks;
 
 namespace GymManagement.DAL.Repositories.Classes
 {
-    internal class TrainerRepository
+    internal class TrainerRepository : IGymUserRepository<Trainer>
+
     {
+        private readonly GymDBContext _dbcontext;
+        public TrainerRepository(GymDBContext gymDB)
+        {
+            _dbcontext = gymDB;
+        }
+        public async Task<int> AddAsync(Trainer user, CancellationToken ct)
+        {
+            _dbcontext.Trainers.Add(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+
+        }
+
+        public async Task<int> DeleteAsync(Trainer user, CancellationToken ct)
+        {
+            _dbcontext.Trainers.Remove(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+
+        }
+
+        public async Task<IEnumerable<Trainer>> GetAllAsync(bool tracking = false, CancellationToken ct = default)
+        {
+            IQueryable<Trainer> res = tracking ? _dbcontext.Trainers : _dbcontext.Trainers.AsNoTracking();
+            return await res.ToListAsync(ct);
+
+        }
+
+        public async Task<Trainer?> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            return await _dbcontext.Trainers.FindAsync(id, ct);
+        }
+
+        public async Task<int> UpdateAsync(Trainer user, CancellationToken ct)
+        {
+            _dbcontext.Trainers.Update(user);
+            return await _dbcontext.SaveChangesAsync(ct);
+        }
     }
+
 }
