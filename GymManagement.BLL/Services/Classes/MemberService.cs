42f48045f9e268784a9d0b9c6c9a14c8ce4f5ed8
jasmine <jasminemohamed174005@gmail.com>
2026-05-25 14:32:51 +0300

    Add Bussiness Logic Layer

---
+++ GymManagement.BLL/Services/Classes/MemberService.cs
@@ -0,0 +1,43 @@
+﻿using GymManagement.BLL.Services.interfaces;
+using GymManagement.BLL.ViewModels.MemberViewModels;
+using GymManagement.DAL.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BLL.Services.Classes
+{
+    internal class MemberService : IMemberService
+    {
+        private readonly IGenericRepository<Member> _repo;
+        public MemberService(IGenericRepository<Member> repo)
+        {
+            _repo = repo;
+            
+        }
+        public async Task<IEnumerable<MemberViewModel>> GetAllMembersAsync(CancellationToken ct = default)
+        {
+          var members =  await _repo.GetAllAsync(ct: ct);
+            if (!members.Any()) return [];
+            else
+            {
+                var memberViewModel = members.Select(m => new MemberViewModel()
+                {
+                    Email = m.Email,
+                    Id = m.Id,
+                    Gender = m.Gender.ToString(),
+                    Phone = m.Phone,
+                    Name = m.Name,
+                    
+
+                });
+
+                return memberViewModel;
+            }
+            
+        }
+    }
+}
---
+++ GymManagement.BLL/Services/interfaces/IMemberService.cs
@@ -0,0 +1,17 @@
+﻿using GymManagement.BLL.ViewModels.MemberViewModels;
+using GymManagement.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BLL.Services.interfaces
+{
+    public interface IMemberService
+    {
+        public Task<IEnumerable<MemberViewModel>> GetAllMembersAsync(CancellationToken ct = default);
+
+        
+    }
+}
