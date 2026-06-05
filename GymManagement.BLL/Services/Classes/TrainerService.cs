075a7b5787eb3a3cbb68fbad8a3db8f0e893f889
jasmine <jasminemohamed174005@gmail.com>
2026-06-05 18:33:22 +0300

    Refactor the trainer to use auto mapper

--- GymManagement.BLL/Services/Classes/TrainerService.cs
+++ GymManagement.BLL/Services/Classes/TrainerService.cs
@@ -1,4 +1,5 @@
-﻿using GymManagement.BLL.Services.interfaces;
+﻿using AutoMapper;
+using GymManagement.BLL.Services.interfaces;
 using GymManagement.BLL.ViewModels.TrainerViewModels;
 using GymManagement.DAL.Models;
 using GymManagement.DAL.Repositories.Interfaces;
@@ -14,10 +15,12 @@ namespace GymManagement.BLL.Services.Classes
     public class TrainerService : ITrainerService
     {
         private readonly IUnitOfWork _unitOfWork;
-        public TrainerService( IUnitOfWork unitOfWork) 
+        private readonly IMapper _mapper;
+
+        public TrainerService( IUnitOfWork unitOfWork , IMapper mapper) 
         {
            _unitOfWork = unitOfWork;
-            
+            this._mapper = mapper;
         }
 
         public async Task<bool> CreateTrainerAsync(TranierToCreateViewModel model, CancellationToken ct = default)
@@ -25,21 +28,7 @@ namespace GymManagement.BLL.Services.Classes
             var EmailExists = await _unitOfWork.GetRepository<Trainer>().AnyAsync(x => x.Email == model.Email, ct);
             var PhoneExists = await _unitOfWork.GetRepository<Trainer>().AnyAsync(x => x.Phone == model.Phone, ct);
             if (EmailExists || PhoneExists) return false;
-            var Trainer = new Trainer()
-            {
-               Phone = model.Phone,
-               Email = model.Email,
-               Gender = model.Gender,
-               speciality = model.Speciality,
-               Name = model.Name,
-               DateOfBirth = model.DateOfBirth,
-               Address = new Address()
-               {
-                   BuildeingNumber = model.BuildingNumber,
-                   City = model.City,
-                   Street = model.Street,
-               }
-            };
+            var Trainer = _mapper.Map<Trainer>(model);
             _unitOfWork.GetRepository<Trainer>().Add(Trainer);
             var res = await _unitOfWork.SaveChangesAsync(ct);
             return res > 0;
@@ -68,16 +57,7 @@ namespace GymManagement.BLL.Services.Classes
             }
             else
             {
-                var res = trainers.Select(x => new TrainerViewModel()
-                {
-                Email = x.Email,
-                Id = x.Id,
-                Name = x.Name,
-                Specialization = x.speciality.ToString(),
-                    Phone = x.Phone
-                    
-                    
-                });
+                var res = _mapper.Map<IEnumerable<Trainer>, IEnumerable<TrainerViewModel>>(trainers);
                 return res;
 
 
@@ -87,19 +67,11 @@ namespace GymManagement.BLL.Services.Classes
 
         }
 
-        public async  Task<TrainerDetailsViewModel?> GetTrainerByIdAsync(int trainerid, CancellationToken ct = default)
+        public async Task<TrainerDetailsViewModel?> GetTrainerByIdAsync(int trainerid, CancellationToken ct = default)
         {
            var trainer =  await _unitOfWork.GetRepository<Trainer>().GetByIdAsync(trainerid , ct );
             if (trainer == null) return null;
-            var model = new TrainerDetailsViewModel()
-            {
-                 Name = trainer.Name,
-                 Phone = trainer.Phone,
-                 Speciality = trainer.speciality.ToString(),
-                 Address = $"{trainer.Address.BuildeingNumber} - {trainer.Address.Street} - {trainer.Address.City}",
-                 DateOfBirth = trainer.DateOfBirth.ToShortDateString(),
-                 Email = trainer.Email,
-            };
+            var model = _mapper.Map<TrainerDetailsViewModel>(trainer);
             return model;
         }
 
@@ -107,17 +79,7 @@ namespace GymManagement.BLL.Services.Classes
         {
            var trainer = await _unitOfWork.GetRepository<Trainer>().GetByIdAsync(trainerid , ct );
             if (trainer == null) return null;
-            var model = new TrainerToUpdateViewModel()
-            {
-                BuildingNumber = trainer.Address.BuildeingNumber,
-                Email = trainer.Email,
-                City = trainer.Address.City,
-                Name = trainer.Name,
-                Phone = trainer.Phone,  
-                Speciality = trainer.speciality,
-                Street = trainer.Address.Street
-
-            };
+            var model = _mapper.Map<Trainer, TrainerToUpdateViewModel>(trainer);
             return model;
 
         }
@@ -129,12 +91,9 @@ namespace GymManagement.BLL.Services.Classes
             var EmailExists = await _unitOfWork.GetRepository<Trainer>().AnyAsync(x => x.Email == model.Email && x.Id != trainerid , ct );
             var PhoneExists = await _unitOfWork.GetRepository<Trainer>().AnyAsync(x => x.Phone == model.Phone && x.Id != trainerid, ct);
             if (EmailExists || PhoneExists) return false;
-            trainer.Email = model.Email;
-            trainer.Phone = model.Phone;
-            trainer.speciality = model.Speciality;
-            trainer.Address.City = model.City;
-            trainer.Address.BuildeingNumber = model.BuildingNumber;
-            trainer.Address.Street = model.Street;
+
+            _mapper.Map(model, trainer);
+            trainer.UpdatedAt = DateTime.Now;
             _unitOfWork.GetRepository<Trainer>().Update(trainer);
             var res = await _unitOfWork.SaveChangesAsync(ct);
             return res > 0;
--- GymManagement.BLL/Services/MappingProfile.cs
+++ GymManagement.BLL/Services/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using GymManagement.BLL.ViewModels.MemberViewModels;
+using GymManagement.BLL.ViewModels.TrainerViewModels;
 using GymManagement.DAL.Models;
 using System;
 using System.Collections.Generic;
@@ -13,10 +14,26 @@ namespace GymManagement.BLL.Services
     {
         public MappingProfile()
         {
+
+            CreateMemberProfiles();
+            CreateTrainerProfiles();
+           
+
+
+
+
+
+
+
+
+        }
+
+        private void CreateMemberProfiles()
+        {
             CreateMap<Member, MemberViewModel>()
-                .ForMember(des => des.Gender, opts => opts.MapFrom(src => src.Gender))
-                .ForMember(des => des.Addresss, opts => opts.MapFrom(src => $"{src.Address.BuildeingNumber} - {src.Address.Street} - {src.Address.City}"))
-                .ForMember(des => des.DateOfBirth, opts => opts.MapFrom(src => src.DateOfBirth));
+               .ForMember(des => des.Gender, opts => opts.MapFrom(src => src.Gender))
+               .ForMember(des => des.Addresss, opts => opts.MapFrom(src => $"{src.Address.BuildeingNumber} - {src.Address.Street} - {src.Address.City}"))
+               .ForMember(des => des.DateOfBirth, opts => opts.MapFrom(src => src.DateOfBirth));
             CreateMap<HealthRecord, HealthRecordViewModel>().ReverseMap();
             CreateMap<Member, MemberToUpdateViewModel>()
                    .ForMember(des => des.Street, opts => opts.MapFrom(src => src.Address.Street))
@@ -29,9 +46,9 @@ namespace GymManagement.BLL.Services
                     //.ForMember(des => des.Address, opts => opts.MapFrom(src => src ))
                     .AfterMap((src, des) =>
                     {
-                         des.Address.Street = src.Street ;
-                         des.Address.BuildeingNumber = src.BuildingNumber ;
-                         des.Address.City = src.City ;
+                        des.Address.Street = src.Street;
+                        des.Address.BuildeingNumber = src.BuildingNumber;
+                        des.Address.City = src.City;
                     });
 
             CreateMap<CreateMemberViewModel, Member>()
@@ -41,12 +58,41 @@ namespace GymManagement.BLL.Services
                      City = src.City,
                      BuildeingNumber = src.BuildingNumber
                  })).ForMember(des => des.HealthRecord, opts => opts.MapFrom(src => src.HealthRecordViewModel));
-                
-            
-                    
 
-                    
-                 
+        }
+        private void CreateTrainerProfiles()
+        {
+            CreateMap<TranierToCreateViewModel, Trainer>()
+               .ForMember(des => des.Address, opts => opts.MapFrom(src => new Address()
+               {
+                   Street = src.Street,
+                   City = src.City,
+                   BuildeingNumber = src.BuildingNumber
+
+               }));
+
+            CreateMap<Trainer, TrainerDetailsViewModel>()
+                .ForMember(des => des.Address, opts => opts.MapFrom(src => $"{src.Address.BuildeingNumber} - {src.Address.Street} - {src.Address.City}"))
+                .ForMember(des => des.DateOfBirth, opts => opts.MapFrom(src => src.DateOfBirth.ToShortDateString()));
+
+
+            CreateMap<Trainer, TrainerViewModel>()
+                .ForMember(des => des.Specialization, opts => opts.MapFrom(src => src.speciality.ToString()))
+
+                ;
+            CreateMap<Trainer, TrainerToUpdateViewModel>()
+                .ForMember(dest => dest.Street, opts => opts.MapFrom(src => src.Address.Street))
+                .ForMember(dest => dest.City, opts => opts.MapFrom(src => src.Address.City))
+                .ForMember(dest => dest.BuildingNumber, opts => opts.MapFrom(src => src.Address.BuildeingNumber));
+
+            CreateMap<TrainerToUpdateViewModel, Trainer>()
+                .AfterMap((src, des) =>
+                { 
+                    des.Address.Street = src.Street;    
+                    des.Address.City = src.City;
+                    des.Address.BuildeingNumber = src.BuildingNumber;
+                })
+                .ForMember(dest => dest.Name , opts => opts.Ignore());
         }
     }
 }
